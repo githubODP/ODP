e2887fb50630c2e441cb6b2fc8938658a501f842
Eduardo <githubodp@gmail.com>
2024-11-11 08:02:21 -0300

    Auto stash before merge of "main" and "origin/main"

--- src/Web/ODP.Web.UI/Models/Corregedoria/InstauracaoViewModel.cs
+++ src/Web/ODP.Web.UI/Models/Corregedoria/InstauracaoViewModel.cs
@@ -9,45 +9,46 @@ namespace ODP.Web.UI.Models.Corregedoria
     public class InstauracaoViewModel
     {
         public Guid Id { get; set; }
-        [Required]
+        
         public int Ano { get; set; }
-        [Display(Name = "Nome Investigado e/ou Empresa")]
-        [Required]
+        [Display(Name = "Investigado")]
+        
         public string Nome { get; set; }
-        [Required]
+       
+        [Display(Name = "CPF/CNPJ")]
         public string CNPJCPF { get; set; }
         public string RG { get; set; }
 
-        [Required]
+        
         [Column(TypeName = "nvarchar(50)")]
         [Display(Name = "Orgão")]
         public ETipoOrgao Orgao { get; set; }
 
-        [Required]
+        
         [Display(Name = "Procedimento")]
 
         [Column(TypeName = "nvarchar(50)")]
         public ETipoProcedimento Procedimento { get; set; }
 
-        [Required]
+        
         [Display(Name = "Protocolo")]
         [Column("Protocolo")]
         public string Protocolo { get; set; }
 
-        [Required]
+       
         public string Objeto { get; set; }
 
-        [Required]
+        
         [Display(Name = "Ato Normativo")]
         [Column("AtoNormativo")]
         public string AtoNormativo { get; set; }
 
-        [Required]
+        
         [Display(Name = "Data Publicação")]
         [Column("DataPublicacao")]
         public DateTime? DataPublicacao { get; set; }
 
-        [Required]
+       
         [Display(Name = "Nº DIOE")]
         [Column("NumeroDIOE")]
         public int? NumeroDIOE { get; set; }
