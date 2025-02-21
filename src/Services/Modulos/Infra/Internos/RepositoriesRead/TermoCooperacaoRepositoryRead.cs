70533303d3bbdc03d3f1ca38526bc618af9e4131
Eduardo <githubodp@gmail.com>
2025-02-21 11:23:40 -0300

    ajustes

--- src/Services/Modulos/Infra/Internos/RepositoriesRead/TermoCooperacaoRepositoryRead.cs
+++ src/Services/Modulos/Infra/Internos/RepositoriesRead/TermoCooperacaoRepositoryRead.cs
@@ -47,10 +47,12 @@ namespace Infra.Internos.RepositoriesRead
 
         public async Task<List<TermoCooperacao>> ListarEnvio()
         {
-            var diasParaAviso = new int[] { 100, 90, 80, 70, 60, 50, 40, 30, 20, 10 }; // Intervalos de alerta
+            var diasParaAviso = new int[] { 100, 90, 80, 70, 60, 50, 40, 30, 20, 10 };
+            var dataAtual = DateTime.Today;
 
             var termos = await _context.TermosCooperacao
-                .Where(t => diasParaAviso.Contains(EF.Functions.DateDiffDay(t.InicioVigencia, t.FimVigencia)))
+                .Where(t => t.FimVigencia > dataAtual &&
+                    diasParaAviso.Contains((t.FimVigencia - dataAtual).Days))
                 .OrderBy(t => t.FimVigencia)
                 .ToListAsync();
 
